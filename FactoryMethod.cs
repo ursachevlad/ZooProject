32e96d57b3f661ed77c75dc95516abc9c6ecff1f
VladMihaiUrsache <ursachevladmihai@yahoo.com>
2019-07-24 14:22:08 +0300

    Add files via upload

---
+++ FactoryMethod.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ZooProject
+{
+    public class MainApp
+    {
+        static void Main()
+        {
+            Creator[] creators = new Creator[2];
+
+            creators[0] = new ConcreteCreatorA();
+            creators[1] = new ConcreteCreatorB();
+
+            foreach (Creator creator in creators)
+            {
+                Product product = creator.FactoryMethod();
+                Console.WriteLine("Created {0}",
+                  product.GetType().Name);
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    abstract class Product
+    {
+    }
+
+    class ConcreteProductA : Product
+    {
+    }
+
+    class ConcreteProductB : Product
+    {
+    }
+
+    abstract class Creator
+    {
+        public abstract Product FactoryMethod();
+    }
+
+    class ConcreteCreatorA : Creator
+    {
+        public override Product FactoryMethod()
+        {
+            return new ConcreteProductA();
+        }
+    }
+
+    class ConcreteCreatorB : Creator
+    {
+        public override Product FactoryMethod()
+        {
+            return new ConcreteProductB();
+        }
+    }
+}

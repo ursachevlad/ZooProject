6629f654db702f772e782a77d4d7bfed0b522d09
Vlad <ursachevladmihai@yahoo.com>
2019-09-09 09:08:37 +0300

    Factory desgin pattern added

---
+++ Zoo/AnimalsActivity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace AnimalsActivity
+{
+    public class Horse : IFactory
+    {
+        public void Drive(int miles)
+        {
+            Console.WriteLine("Horse activity in a day is : " + miles.ToString() + "km");
+        }
+    }
+
+    public class Dog : IFactory
+    {
+        public void Drive(int miles)
+        {
+            Console.WriteLine("Dog activity in 24h is: " + miles.ToString() + "km");
+        }
+    }
+
+    public abstract class Animal
+    {
+        public abstract IFactory GetAnimal(string Animal);
+    }
+
+    public class ConcreteAnimal : Animal
+    {
+        public override IFactory Animal(string Animal)
+        {
+            switch (Animal)
+            {
+                case "Horse":
+                    return new Horse();
+                case "Dog":
+                    return new Dog();
+                default:
+                    throw new ApplicationException(string.Format("Animal '{0}' cannot be created", Animal));
+            }
+        }
+    }
+}
